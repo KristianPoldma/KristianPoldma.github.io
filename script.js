67106dc10de0e0d0df73a9ad2e1a7cdf15940896
KristianPoldma <95612858+KristianPoldma@users.noreply.github.com>
2024-11-13 00:05:04 +0200

    esmane timer

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
-function playTime(id) {
-    console.log(id)
-}
+const now = new Date();
+const date = now.getDate();
+const month = now.getMonth();
+const hours = now.getHours();
+const minutes = now.getMinutes();
+
+//TIMER variables
+let elapsedTime = 0;
+let timerInterval;
+let timerRunning = false;
+
+
+
+function start() {
+    if(!timerRunning) {
+        timerInterval = setInterval(() => {
+            elapsedTime++
+            document.getElementById('timer').innerHTML = elapsedTime;
+        }, 1000);
+        timerRunning = true;
+    }
+};
+
+function pause() {
+    clearInterval(timerInterval);
+    timerRunning = false;
+};
+
+function reset() {
+    clearInterval(timerInterval);
+    elapsedTime = 0;
+    timerRunning = false;
+    document.getElementById('timer').innerHTML = elapsedTime;
+};
+
+
+document.getElementById("playButton").addEventListener("click", start);
+
+document.getElementById("pauseButton").addEventListener("click", pause);
+  document.getElementById("stopButton").addEventListener("click", reset);
